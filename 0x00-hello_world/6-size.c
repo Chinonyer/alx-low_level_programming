fbce4161ad30b30904dbe558c646aab610a3fa1c
Chinonyer <victoriachinonye22@gmail.com>
2022-12-09 07:53:29 -0800

    giiit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints a given phrase
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+	int intype;
+	float floatType;
+	double doubleType;
+	char charType;
+
+
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of long int: %zu bytes\n", sizeof(longintType));
+	printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
+	return (0);
+}
