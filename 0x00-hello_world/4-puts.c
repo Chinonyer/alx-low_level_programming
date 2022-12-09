3eecaf50e04d53bcd1cb3215181c6d4e23f63c08
Chinonyer <victoriachinonye22@gmail.com>
2022-12-09 06:49:52 -0800

    do

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 
 /**
  * main - prints a given phrase
- * Return: 0 if exited properly, non-zero-otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
