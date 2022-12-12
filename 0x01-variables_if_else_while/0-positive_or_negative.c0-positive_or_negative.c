35947bcbd250da513f8b3d9ad9a64c3d2a665da6
Chinonyer <victoriachinonye22@gmail.com>
2022-12-12 03:23:33 -0800

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c0-positive_or_negative.c
@@ -7,7 +7,7 @@
 /**
  * main - Entry point
  *
- * Return; Always 0  (success)
+ *Return: Always 0 (success)
  */
 
 int main(void)
@@ -17,14 +17,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if  (n > 0)
-		 printf("%d is positive\n", n);
+	if (n > 0)
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-	return (o);
+	return (0);
 }	
 
 
- 
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 ‘0x01-vairiables_if_else_while
+alx tasks
