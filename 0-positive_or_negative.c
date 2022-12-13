75792fd652db797b24df8109fed75033eea2c8ce
Chinonyer <victoriachinonye22@gmail.com>
2022-12-13 04:24:28 -0800

    food

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -3,27 +3,24 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
- *
- *Return: Always 0 (success)
+ * Description: prints if random generated number is positive,
+ * zero or negative
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
-		printf("%d is negative\n", n);
+		printf("%d is positive\n", n);
 	return (0);
-}	
-
+}
 
